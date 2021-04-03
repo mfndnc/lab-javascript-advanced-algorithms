27bbbfacb5956155c3b0db7e21e7c1449c9be3ea
Marcia Faria <mfndnc@gmail.com>
2021-04-03 18:34:24 +0200

    done

--- js/queue/queue-dom.js
+++ js/queue/queue-dom.js
@@ -1,36 +1,67 @@
 const queueUL = document.querySelector('.list-queue');
 const queueInput = document.querySelector('.queue-input');
 const warningTopQueue = document.querySelector('#queue-container .warning-top');
-const warningBottomQueue = document.querySelector('#queue-container .warning-bottom');
+const warningBottomQueue = document.querySelector(
+  '#queue-container .warning-bottom'
+);
 const addQueue = document.querySelector('.btn-add-queue');
 const dequeue = document.querySelector('.btn-take-dequeue');
 
 const queue = new QueueDataStructure();
 
 const clearQueueInput = () => {
-  // ... your code goes here
+  queueInput.value = '';
 };
 
 const generateListQueue = () => {
-  // ... your code goes here
+  warningTopQueue.style.display = 'none';
+  warningBottomQueue.style.display = 'none';
+
+  queueUL.innerHTML = '';
+
+  let dataArr = queue.display();
+  dataArr.forEach((item) => {
+    let li = document.createElement('li');
+    li.className = 'active';
+    li.innerText = item;
+    queueUL.appendChild(li);
+  });
+  for (let i = dataArr.length; i < queue.MAX_SIZE; i++) {
+    let li = document.createElement('li');
+    li.className = 'inactive';
+    queueUL.appendChild(li);
+  }
 };
 
 generateListQueue();
 
-const generateWarningQueue = type => {
+const generateWarningQueue = (type) => {
   if (type === 'underflow') {
-    // ... your code goes here
+    warningBottomQueue.innerText = 'underflow';
+    warningBottomQueue.style.display = 'block';
   } else if (type === 'overflow') {
-    // ... your code goes here
+    warningTopQueue.innerText = 'overflow';
+    warningTopQueue.style.display = 'block';
   }
 };
 
 const addToQueue = () => {
-  // ... your code goes here
+  let entry = queue.enqueue(queueInput.value);
+  clearQueueInput();
+  if (typeof entry == 'string') {
+    generateWarningQueue('overflow');
+  } else {
+    generateListQueue();
+  }
 };
 
 const removeFromQueue = () => {
-  // ... your code goes here
+  if (queue.isEmpty()) {
+    generateWarningQueue('underflow');
+  } else {
+    queue.dequeue();
+    generateListQueue();
+  }
 };
 
 addQueue.addEventListener('click', addToQueue);
--- js/stack/stack-dom.js
+++ js/stack/stack-dom.js
@@ -2,36 +2,67 @@ const stackList = document.getElementById('stack-list');
 const stackInput = document.getElementById('stack-input');
 const container = document.getElementById('container');
 const warningTopStack = document.querySelector('#stack-container .warning-top');
-const warningBottomStack = document.querySelector('#stack-container .warning-bottom');
+const warningBottomStack = document.querySelector(
+  '#stack-container .warning-bottom'
+);
 const addStackBtn = document.getElementById('add-stack');
 const takeStackBtn = document.getElementById('take-stack');
 
 const newStack = new StackDataStructure();
 
 const clearStackInput = () => {
-  // ... your code goes here
+  stackInput.value = '';
 };
 
 const renderListStack = () => {
-  // ... your code goes here
+  warningTopStack.style.display = 'none';
+  warningBottomStack.style.display = 'none';
+
+  stackList.innerHTML = '';
+
+  let dataArr = newStack.display();
+  dataArr.forEach((item) => {
+    let li = document.createElement('li');
+    li.className = 'active';
+    li.innerText = item;
+    stackList.appendChild(li);
+  });
+  for (let i = dataArr.length; i < newStack.MAX_SIZE; i++) {
+    let li = document.createElement('li');
+    li.className = 'inactive';
+    stackList.appendChild(li);
+  }
 };
 
 renderListStack();
 
-const generateWarningStack = type => {
+const generateWarningStack = (type) => {
   if (type === 'underflow') {
-    // ... your code goes here
+    warningBottomStack.innerText = 'underflow';
+    warningBottomStack.style.display = 'block';
   } else if (type === 'overflow') {
-    // ... your code goes here
+    warningTopStack.innerText = 'overflow';
+    warningTopStack.style.display = 'block';
   }
 };
 
 const addToStack = () => {
-  // ... your code goes here
+  let entry = newStack.push(stackInput.value);
+  clearStackInput();
+  if (typeof entry == 'string') {
+    generateWarningStack('overflow');
+  } else {
+    renderListStack();
+  }
 };
 
 const removeFromStack = () => {
-  // ... your code goes here
+  if (newStack.isEmpty()) {
+    generateWarningStack('underflow');
+  } else {
+    newStack.pop();
+    renderListStack();
+  }
 };
 
 addStackBtn.addEventListener('click', addToStack);
